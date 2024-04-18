7c1b0cb36d472a91a40469d911ed727fb2b8d339
Dhohir Pradana <dhohirkudus@gmail.com>
2024-04-18 13:28:41 +0700

    health check

--- app.py
+++ app.py
@@ -27,5 +27,10 @@ def test_conn():
     return test_conn_handler(request, jsonify)
 
 
+@app.route('/health')
+def health():
+    return jsonify("OK"), 200
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
